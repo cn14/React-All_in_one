e55eb17bec0cbe0d1331047905a8733929cb3dd9
cn14 <chethan.171co113@nitk.edu.in>
2020-07-26 17:14:03 +0530

    Updates

---
+++ precache-manifest.3128365241fed71ec1aa3d1ce9ef401f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "665cd6a816e74481a1438883e31af39d",
+    "url": "/React-All_in_one/index.html"
+  },
+  {
+    "revision": "44d130c4c1553d4e741e",
+    "url": "/React-All_in_one/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "9ba2f4b6c4c92aa8ea07",
+    "url": "/React-All_in_one/static/css/main.303705d9.chunk.css"
+  },
+  {
+    "revision": "44d130c4c1553d4e741e",
+    "url": "/React-All_in_one/static/js/2.67ccfcc5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/React-All_in_one/static/js/2.67ccfcc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ba2f4b6c4c92aa8ea07",
+    "url": "/React-All_in_one/static/js/main.0d2b8716.chunk.js"
+  },
+  {
+    "revision": "ba2d11662288d4aad3a8",
+    "url": "/React-All_in_one/static/js/runtime-main.cebb5c7a.js"
+  },
+  {
+    "revision": "b46b36cba686665d0c91bd740e61a2da",
+    "url": "/React-All_in_one/static/media/about.b46b36cb.png"
+  },
+  {
+    "revision": "3e257af80c95d3e7c6e30b7a046ff8fc",
+    "url": "/React-All_in_one/static/media/welcome.3e257af8.png"
+  }
+]);
